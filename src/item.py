f9b7a2d556a010da8cdb8c8eb848a1efe2e30d19
evgen <evgen1973108@gmail.com>
2024-01-27 00:28:34 +0300

    add item

--- src/item.py
+++ src/item.py
@@ -5,26 +5,51 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int)-> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        pass
+        all_price = self.price * self.quantity
+        return all_price
+
+
+
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
+
+
+if __name__ == '__main__':
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    print(item1.calculate_total_price())  # 200000
+    print(item2.calculate_total_price())  # 100000
+
+    # устанавливаем новый уровень цен
+    Item.pay_rate = 0.8
+    # применяем скидку
+    item1.apply_discount()
+
+    print(item1.price)  # 8000.0
+    print(item2.price)  # 20000
+
+
+

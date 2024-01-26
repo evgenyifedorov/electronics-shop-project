6b2a41a2a84cd6fc645bbf0d8cd61f78897891e3
evgen <evgen1973108@gmail.com>
2024-01-27 00:29:38 +0300

    add test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
+import pytest
+
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+@pytest.fixture
+def item_for_test():
+    return Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price(item_for_test):
+    assert item_for_test.calculate_total_price() == 200000
+
+def test_apply_discount(item_for_test):
+    item_for_test.apply_discount()
+    assert item_for_test.price * item_for_test.pay_rate == item_for_test.price

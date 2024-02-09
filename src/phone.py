c660dd3bc46661f2d0d86720e0ea2f44cb01028a
evgen <evgen1973108@gmail.com>
2024-02-09 22:07:30 +0300

    add phone

--- src/phone.py
+++ src/phone.py
@@ -1,14 +1,9 @@
-from item import Item
+from src.item import Item
 
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim):
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
-
-    def add(self,other):
-        if not isinstance(other,Item):
-            raise ValueError
-        return self.phone1 + other.item1
 
 
     @property
@@ -16,10 +11,16 @@ class Phone(Item):
         return self.__number_of_sim
 
     @number_of_sim.setter
-    def number_of_sim(self, number_of_sim, number):
-        if number <= 0:
+    def number_of_sim(self, number):
+        if int(number) <= 0:
             raise ValueError ("Количество физических SIM-карт должно быть целым числом больше нуля.")
         else:
             self.__number_of_sim = int(number)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} ('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self):
+        return f"{self.name}"
 
 
